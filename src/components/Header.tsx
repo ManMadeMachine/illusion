9881f020ad6c2bc197dbaf6897adb2e7fe68fd68
Eetu <Eetu.Suonpaa@hotmail.com>
2020-01-15 09:06:26 +0200

    little more styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = ({linkItems} :  Props) => {
 
     return (
         <nav className="flex items-center justify-between flex-wrap pt-2 pb-2 bg-gray-900 text-white p-6">
-            <h1 className="flex text-xl">Illusion</h1>
+            <h1 className="flex text-xl mr-5">Illusion</h1>
 
             {/* Small-size top-menu button*/}
             <div className="block md:hidden">
@@ -24,7 +24,8 @@ const Header = ({linkItems} :  Props) => {
             {/* Actual menu items */}
             <ul className={`${navbarState ? '' : 'hidden'} w-full flex-grow md:flex md:flex-row md:w-auto`}>
                 {linkItems.map(item => {
-                        return(<li key={item} className="flex p-2"><Link to={'/' + item.toLowerCase()}>{item}</Link></li>);
+                        // TODO: Item as a component
+                        return(<li key={item} className="flex p-2"><Link to={'/' + item.toLowerCase()} className="text-gray-500 hover:text-gray-300">{item}</Link></li>);
                     })
                 }
             </ul>
