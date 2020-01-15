fbda76a252f9b002cb0726fa87a00e2fa2f636ca
Eetu <Eetu.Suonpaa@hotmail.com>
2020-01-15 09:02:29 +0200

    it works!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 
-import './Header.css';
-
 interface Props {
     linkItems: string[]
 }
@@ -12,31 +10,21 @@ const Header = ({linkItems} :  Props) => {
 
     const toggleNavBar = () => {
         setNavbarState(!navbarState);
-        console.log("Toggle navbar ", (navbarState ? 'on' : 'off'));
     };
 
     return (
         <nav className="flex items-center justify-between flex-wrap pt-2 pb-2 bg-gray-900 text-white p-6">
             <h1 className="flex text-xl">Illusion</h1>
 
-            {/* Small-size top-menu */}
+            {/* Small-size top-menu button*/}
             <div className="block md:hidden">
-                {/* <span className="align-middle px-2" >-</span> */}
-                <button className="flex items-center" onClick={() => toggleNavBar()}>--</button>
+                <button className="flex items-center p-2 bg-gray-700" onClick={() => toggleNavBar()}>--</button>
             </div>
-
-            {/* { navbarState &&    
-                <div className="md:hidden absolute top-0 right-0 bg-gray-800 w-full">
-                    {linkItems.map(item => {
-                        return(<li key={item} className=""><Link to={'/' + item.toLowerCase()}>{item}</Link></li>);
-                    })
-                }
-                </div>
-            } */}
-
-            <ul className={`${navbarState ? '' : 'hidden'} flex-grow md:flex md:flex-row md:w-auto`}>
+            
+            {/* Actual menu items */}
+            <ul className={`${navbarState ? '' : 'hidden'} w-full flex-grow md:flex md:flex-row md:w-auto`}>
                 {linkItems.map(item => {
-                        return(<li key={item} className="flex p-2"><Link to={'/' + item.toLowerCase()}>{item}</Link></li>); // TODO: item string as link path
+                        return(<li key={item} className="flex p-2"><Link to={'/' + item.toLowerCase()}>{item}</Link></li>);
                     })
                 }
             </ul>
